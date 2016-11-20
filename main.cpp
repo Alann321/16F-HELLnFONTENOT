f89a22872737e3948726d6b03a28231843b648f5
ForeverABoy <seungkil@smu.edu>
2016-11-20 13:33:48 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "FileManager.h"
+
+int main (int argc, char* const argv[])
+{
+  //call functions from fileManager
+  return 0;
+}
